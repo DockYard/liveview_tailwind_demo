32d8202b76a09beba9c33ebae1d931b17fef921a
Bennie Rosas <ben@aliencyb.org>
2021-12-15 00:51:39 -0600

    Further styling with Tailwind components; Customize Tailwind animations; remove remaining CSS code.

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -4,7 +4,32 @@ module.exports = {
     '../lib/*_web/**/*.*ex'
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'fade-in-scale': '0.3s ease-in normal forwards 1 fade-in-scale',
+        'fade-out-scale': '0.3s ease-out normal forwards 1 fade-out-scale',
+        'fade-in': '0.3s ease-in 0s normal forwards 1 fade-in',
+        'fade-out': '0.3s ease-out 0s normal forwards 1 fade-out'
+      },
+      keyframes: {
+        'fade-in-scale': {
+          '0%': { transform: 'scale(0.95)', opacity: 0 },
+          '100%': { transform: 'scale(1.0)', opacity: 1 }
+        },
+        'fade-out-scale': {
+          '0%': { transform: 'scale(1.0)', opacity: 1 },
+          '100%': { transform: 'scale(0.95)', opacity: 0 }
+        },
+        'fade-in': {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 }
+        },
+        'fade-out': {
+          '0%': { opacity: 1 },
+          '100%': { opacity: 0 }
+        }
+      }
+    },
   },
   plugins: [],
 }
